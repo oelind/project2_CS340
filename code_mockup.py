b31b4ddc43f39e3674e2c8b393312c287c48f873
Alice Lindsay <beamgirl490@gmail.com>
2026-03-07 18:36:58 -0500

    swapped bugs and specifically have done research on how hex values can be stored in python and why the answer returmed is technically coeect in some contexts but not this one

--- code_mockup.py
+++ code_mockup.py
@@ -1,6 +1,27 @@
-#case of one of lists/ recieved params is empty
-def one_empty_params(list_1, list_2, result_list):
+#hex codes missing leading zeros
 
-#if list one empty
-    if(list_1.len < 1): # result should be the contents of the second param
-        return (list_2 == result_list)
+#https://colorwikia.com/blog/how-hex-color-codes-work/
+
+#--output missing 0s if value needs only one value-space
+# (too condensed 3 digit hex-codes imply there are doubles
+#of that value ie a would be aa -- ABC => AABBCC so while at times
+#the leading nums can be condensed other times it causes
+# the hex-code to be incorrect)
+
+#https://python-reference.readthedocs.io/en/latest/docs/float/hex.html
+#using hex() returns a string of the number as its floating int equivelent--> returns string type specifically so may not
+#be what is causing this error
+
+#or that typically hex is used to store compressed binary so what might be returned is the value as is
+#and not the appropoiate format for the hex version of RGB values 
+#(in other words the nost condenced version of the nums are returned so the bug may be in the 
+#formatting of the three values in hex in order while not being a single value as the default might be)
+
+#Using the prefix 0x cann allow for literal values to be stored in hex
+
+
+
+
+
+
+
